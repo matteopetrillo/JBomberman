7e8788b9249c2cebf8dea591fe0e9a83383ec0a8
Matteo Petrillo <commercial@matteopetrillo.info>
2023-08-19 15:47:18 +0200

    last fixes

--- src/main/java/it/petrillo/jbomberman/JBomberMan.java
+++ src/main/java/it/petrillo/jbomberman/JBomberMan.java
@@ -4,7 +4,7 @@ import it.petrillo.jbomberman.controller.GameManager;
 
 /**
  * The main class of the JBomberMan game application.
- * This class contains the main method that initializes and starts the game
+ * This class contains the main method that initializes and open the game's menu
  * by invoking the openMenu() method of the GameManager.
  */
 public class JBomberMan {
